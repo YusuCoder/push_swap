1c2f5ca13b485c2d15eef679eaa1dc1e2dc9090c
Rustamjon Yusupov <ryusupov@2-D-5.42heilbronn.de>
2024-05-03 11:47:42 +0200

    created a file called stacks to create some helper functions for stack job, and created a function buttom stack to create a new node to the buttom of the list

--- stacks.c
+++ stacks.c
@@ -6,49 +6,63 @@
 /*   By: ryusupov <ryusupov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 11:44:33 by ryusupov          #+#    #+#             */
-/*   Updated: 2024/05/01 12:18:27 by ryusupov         ###   ########.fr       */
+/*   Updated: 2024/05/03 11:46:36 by ryusupov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./ryusupov_h/ryusupov.h"
 
-t_ryusupov	*get_buttom_stack(t_ryusupov *r_stack)
-{
-	while (r_stack && r_stack->next != NULL)
-	{
-		r_stack = r_stack->next;
-	}
-	return (r_stack);
-}
+/*Function buttom_stack adds new node to the buttom of the list*/
 
-void	buttom_stack(t_ryusupov **r_stach_a, t_ryusupov *new)
+void	buttom_stack(t_ryusupov **r_stack_a, t_ryusupov *new)
 {
-	t_ryusupov	*tail;
+	t_ryusupov	*current;
 
 	if (!new)
 		exit(EXIT_FAILURE);
-	if (!*r_stach_a)
+	if (!*r_stack_a)
 	{
-		*r_stach_a = new;
+		*r_stack_a = new;
 		return ;
 	}
-	tail = get_bottom_stack(*r_stach_a);
-	tail->next = new;
+	current = *r_stack_a;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = new;
 }
 
-t_ryusupov	*stack_new(int str)
-{
-	t_ryusupov	*new;
+// int	main(void)
+// {
+// 	t_ryusupov	*node1;
+// 	t_ryusupov	*node2;
+// 	t_ryusupov	*node3;
+// 	t_ryusupov	*stack;
+// 	t_ryusupov	*current;
 
-	new = malloc(sizeof(t_ryusupov));
-	if (!new)
-		return (NULL);
-	new->r_content;
-	new->i = 0;
-	new->r_pos = -1;
-	new->r_target_pos = -1;
-	new->r_cost_a = -1;
-	new->r_cost_a = -1;
-	new->next = NULL;
-	return (new);
-}
+// 	node1 = (t_ryusupov *)malloc(sizeof(t_ryusupov));
+// 	node2 = (t_ryusupov *)malloc(sizeof(t_ryusupov));
+// 	node3 = (t_ryusupov *)malloc(sizeof(t_ryusupov));
+// 	node1->r_content = 1;
+// 	node1->next = NULL;
+// 	node2->r_content = 2;
+// 	node2->next = NULL;
+// 	node3->r_content = 3;
+// 	node3->next = NULL;
+// 	stack = NULL;
+// 	buttom_stack(&stack, node1);
+// 	buttom_stack(&stack, node2);
+// 	buttom_stack(&stack, node3);
+// 	printf("Stack contents:\n");
+// 	current = stack;
+// 	while (current != NULL)
+// 	{
+// 		printf("%d\n", current->r_content);
+// 		current = current->next;
+// 	}
+// 	free(node1);
+// 	free(node2);
+// 	free(node3);
+// 	return (0);
+// }
